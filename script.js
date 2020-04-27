ef3a0953b13fe5f972769639a1fe5f095258c3d6
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-27 19:48:38 +0200

    Imoprove contact page

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+function switchButton(status){
+  var button = document.getElementById('send');
+  if (status) {
+	button.disabled = false;
+  }else{
+	button.disabled = true;
+  }
+}
+
 function checkData() {
   var email = document.getElementById("email").value.indexOf("@");
   var message = document.getElementById("message").value;
   var name = document.getElementById("name").value;
   submitOK = true;
 
-  if (name.length < 1) {
-    console.log("The name is empty");
-    submitOK = false;
-  } 
-
+  var patt = new RegExp("([A-Z])\\w+");
+  var res = patt.test(name);
+	console.log(res);
+	if(!res){
+		switchButton(res);
+		return;
+	}
+  
   if (message.length < 1 ) {
     console.log("The message is empty");
 	console.log(message);
@@ -20,12 +32,11 @@ function checkData() {
     submitOK = false;
   }
 
-  if (submitOK) {
-    const button = document.getElementById('send');
-	button.disabled = false;
-  }else{
-	const button = document.getElementById('send');
-	button.disabled = true;
-  }
+  switchButton(submitOK);
 	console.log(submitOK);
 }
+
+function displayMessage(){
+	alert("You send a data");	
+}
+
